0bd5363846af63d2d5af185d3e086b057cafb8ea
fallenangel3k <fallenangel3k@googlemail.com>
2016-08-29 06:41:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LevqigTAAAAAPSTvIbdlxhf1wDud851uD4dziFQ', 
-  redirect_uri: 'https://untitled-dice.github.io',
+  redirect_uri: 'https://fallenangel3k.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
